68f9bb4969eefb974e18a5e4e24c974792a3ec46
Namco0816 <419285506@qq.com>
2019-03-19 23:54:09 +0800

    folder merge

--- markov/utils/test.py
+++
@@ -1,4 +0,0 @@
-import load_HMM_data
-
-a,b,c,d = load_HMM_data.load_hmm_data("../data/example.hmm")
-print(a,b,c,d)
--- markov/utils/test2.py
+++
@@ -1,4 +0,0 @@
-with open("../data/example.fa") as f:
-    data = [line.strip() for line in f]
-new_data = "".join(data)
-print(list(new_data))
