eea55a8f35bd346952d5375e884087ed2be38db1
Namco0816 <419285506@qq.com>
2019-03-22 20:45:17 +0800

    add some comment

--- markov/markovChain.py
+++ markov/markovChain.py
@@ -3,6 +3,15 @@ import utils.load_data as ld
 
 class MarkovChain(object):
     def __init__(self, input_data, inside_model, outside_model):
+        '''
+        @function:
+        to initialize the Markov Chain model, this function will load the input data,
+        the inside probability table, and the outside one
+        @params:
+        input_data, the path of the test input samples
+        inside_model: the path of the inside probability table
+        outside_model: the path of the outside probability table
+        '''
         self.inside_transition_list = ld.load_transition_table(inside_model)
         self.outside_transition_list = ld.load_transition_table(outside_model)
 
@@ -12,6 +21,10 @@ class MarkovChain(object):
         self.input_list = ld.load_input_file(input_data)
 
     def get_prob_list(self):
+        '''
+        @func:
+        to generate the result of the inside_score minus the outside_score
+        '''
         inside_result = []
         inside_temp_result = []
         outside_result = []
@@ -37,6 +50,10 @@ class MarkovChain(object):
         self.result = [inside[i]- outside[i] for i in range(0, len(inside))]
         return self.result
     def get_inside_or_outside(self):
+        """
+        @func:
+        after the calculation of the prob_list, generate the final result whether inside of outside
+        """
         self.classification_result = []
         for sub in self.result:
             if sub>0:
@@ -46,6 +63,10 @@ class MarkovChain(object):
             self.classification_result.append(value)
         return self.classification_result
     def generate_final_result(self):
+        """
+        @func:
+            zip all the final result into a dict
+        """
         result_dict = dict(zip(self.result, self.classification_result))
         return result_dict
 
